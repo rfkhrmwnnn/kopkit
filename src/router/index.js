b20b36c30008b71a27f77299e0b94f325ea496fe
unknown <vincareflava@gmail.com>
2026-01-28 09:45:21 +0700

    update no splash

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import SplashView from '../views/SplashView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'splash',
-      component: SplashView
+      redirect: '/login'
     },
     {
       path: '/login',
